567fe6f1faba570ccef1d437ad67a4b8389b461e
asmaa-ezz <asmadhoun@gmail.com>
2019-06-22 23:01:36 +0300

    Add the simple commponent main page

---
+++ client/src/components/main/ResetPassword.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class ResetPassword extends Component {
+  render() {
+    return (
+      <div>
+        Page reset password
+        {/* form conect - input enail */}
+      </div>
+    );
+  }
+}
+
+export default ResetPassword;
