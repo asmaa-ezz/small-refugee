c43764313ed430734375fd3fbbb2ef73ff1d9404
asmaa-ezz <asmadhoun@gmail.com>
2019-06-23 01:25:33 +0300

    Remove footer

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,6 @@ import React, { Component } from "react";
 import { BrowserRouter } from "react-router-dom";
 import Header from "./components/Header";
 import Main from "./components/main";
-import Footer from "./components/Footer";
 
 class App extends Component {
   render() {
@@ -11,7 +10,6 @@ class App extends Component {
         <BrowserRouter>
           <Header />
           <Main />
-          <Footer />
         </BrowserRouter>
       </React.Fragment>
     );
