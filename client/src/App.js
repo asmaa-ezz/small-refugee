a14f392142d7f586930c78215cf32a1cab0dc53a
asmaa-ezz <asmadhoun@gmail.com>
2019-06-22 22:46:10 +0300

    Add the component in the App

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,21 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import { BrowserRouter } from "react-router-dom";
+import Header from "./components/Header";
+// import Main from "./components/Main.jsx";
+import Footer from "./components/Footer";
 
 class App extends Component {
   render() {
     return (
-      <div>
-        App
-      </div>
-    )
+      <React.Fragment>
+        <BrowserRouter>
+          <Header />
+          {/* <Main /> */}
+          <Footer />
+        </BrowserRouter>
+      </React.Fragment>
+    );
   }
 }
 
-export default App
+export default App;
