c54481b827511c2995d19800e4404a0e7d0ab920
asmaa-ezz <asmadhoun@gmail.com>
2019-06-23 01:29:01 +0300

    Add the sign out remove token

--- client/src/components/SignOut.jsx
+++ client/src/components/SignOut.jsx
@@ -1,9 +1,23 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
+import { LogOutRemoveToken } from "../store/action/actionCreator/actionAuth";
 
 class SignOut extends Component {
+  componentDidMount() {
+    this.props.LogOutRemoveToken();
+  }
   render() {
-    return <div />;
+    return null;
   }
 }
 
-export default SignOut;
+const mapDispatchToProps = dispatch => {
+  return {
+    LogOutRemoveToken: () => dispatch(LogOutRemoveToken())
+  };
+};
+
+export default connect(
+  null,
+  mapDispatchToProps
+)(SignOut);
