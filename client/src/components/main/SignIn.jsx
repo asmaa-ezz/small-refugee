1896521d3d2e94a4caca22e63c26ef0a8ed9289f
asmaa-ezz <asmadhoun@gmail.com>
2019-06-23 00:32:48 +0300

    Crete sign in form use * formik * bootstrap style * yup validation

--- client/src/components/main/SignIn.jsx
+++ client/src/components/main/SignIn.jsx
@@ -1,8 +1,90 @@
 import React, { Component } from "react";
+import { Formik, Field } from "formik";
+import * as Yup from "yup";
+import styled from "styled-components";
+
+const FormStyle = styled.form`
+  margin-top: 10%;
+  border: 1px solid #ccc;
+  border-radius: 10px;
+  padding: 5%;
+  outline: none;
+`;
+
+const userSchema = Yup.object().shape({
+  email: Yup.string()
+    .email()
+    .required(),
+  password: Yup.string()
+    .required()
+    .max(20)
+    .min(1)
+});
 
 class SignIn extends Component {
   render() {
-    return <div />;
+    return (
+      <Formik
+        initialValues={{ email: "", password: "" }}
+        validationSchema={userSchema}
+        onSubmit={values => {
+          console.log(values);
+        }}
+      >
+        {props => (
+          <FormStyle onSubmit={props.handleSubmit}>
+            <div className="form-group">
+              <label htmlFor="email">Email address</label>
+              <Field
+                type="email"
+                placeholder="Enter email"
+                onChange={props.handleChange}
+                name="email"
+                value={props.values.email}
+                className="form-control"
+              />
+              {props.errors.email && props.touched.email ? (
+                <span className="form-text text-danger small">
+                  {props.errors.email}
+                </span>
+              ) : (
+                ""
+              )}
+            </div>
+
+            <div className="form-group">
+              <label htmlFor="password">Password</label>
+              <Field
+                type="password"
+                onChange={props.handleChange}
+                name="password"
+                value={props.values.password}
+                placeholder="Password"
+                className="form-control"
+              />
+              {props.errors.password && props.touched.password ? (
+                <span className="form-text text-danger small">
+                  {props.errors.password}
+                </span>
+              ) : (
+                ""
+              )}
+            </div>
+
+            <button
+              type="submit"
+              disabled={!props.dirty && !props.isSubmitting}
+              className="btn btn-primary"
+            >
+              Login
+            </button>
+            <button type="button" className="btn btn-link">
+              Reset Password
+            </button>
+          </FormStyle>
+        )}
+      </Formik>
+    );
   }
 }
 
