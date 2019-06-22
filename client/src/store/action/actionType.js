56d7c5234dcf56f80ba1342adab4d2df99f56c4d
asmaa-ezz <asmadhoun@gmail.com>
2019-06-22 23:10:35 +0300

    Add some action type of auth

---
+++ client/src/store/action/actionType.js
@@ -0,0 +1,7 @@
+// Action of Auth
+export const SIGNIN_SUCCESS = 'SIGNIN_SUCCESS';
+export const SIGNIN_ERROR = 'SIGNIN_ERROR';
+export const SIGNUP_SUCCESS = 'SIGNUP_SUCCESS';
+export const SIGNUP_ERROR = 'SIGNUP_ERROR';
+export const SIGNOUT_SUCCESS = 'SIGNOUT_SUCCESS';
+export const RESET_PASSWORD = 'RESET_PASSWORD';
