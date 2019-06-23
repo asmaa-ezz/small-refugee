912032483362c99d9616b811d26d2ec17d9e79da
asmaa-ezz <asmadhoun@gmail.com>
2019-06-23 12:25:25 +0300

    Add some action require to action post

--- client/src/store/action/actionTypes.js
+++ client/src/store/action/actionTypes.js
@@ -6,3 +6,13 @@ export const SIGNUP_ERROR = 'SIGNUP_ERROR';
 export const SIGNOUT_SUCCESS = 'SIGNOUT_SUCCESS';
 export const RESET_PASSWORD = 'RESET_PASSWORD';
 export const DECODE_TOKEN = 'DECODE_TOKEN';
+
+// Action of Post
+
+export const GET_ALL_SUBJECT = 'GET_ALL_SUBJECT';
+export const ADD_POST = 'ADD_POST';
+export const GET_POSTS = 'GET_POSTS';
+export const GET_POSTS_PROFILE = 'GET_POSTS_PROFILE';
+
+
+
