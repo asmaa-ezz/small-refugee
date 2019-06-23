a765d95f95be99a0ae5f3908f643f68648bffddb
asmaa-ezz <asmadhoun@gmail.com>
2019-06-23 13:46:13 +0300

    Add the create post of home page

--- client/src/components/main/Home.jsx
+++ client/src/components/main/Home.jsx
@@ -1,8 +1,14 @@
 import React, { Component } from "react";
+import Post from "../post";
 
 class Home extends Component {
   render() {
-    return <div>Home Page</div>;
+    return (
+      <React.Fragment>
+        <div>Home Page</div>
+        <Post />
+      </React.Fragment>
+    );
   }
 }
 
