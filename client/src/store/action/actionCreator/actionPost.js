4afb6fdf764061e9ba8a153d7924d1a5e967a9e7
asmaa-ezz <asmadhoun@gmail.com>
2019-06-23 13:01:30 +0300

    Add the action get all subject

---
+++ client/src/store/action/actionCreator/actionPost.js
@@ -0,0 +1,28 @@
+import { GET_ALL_SUBJECT, ADD_POST, GET_POSTS, GET_POSTS_PROFILE } from '../actionTypes';
+
+export const GetToken = () => {
+  const jwt = localStorage.getItem("token");
+  return jwt;
+}
+
+export const GetAllSubject = () => dispatch => {
+  const proxyurl = 'https://cors-anywhere.herokuapp.com/';
+  const url = 'https://small-refugee-app.herokuapp.com/main/subject/';
+
+  const Token = GetToken();
+
+  fetch((proxyurl + url), {
+    headers: {
+      'content-type': 'application/json',
+      "Authorization": `JWT ${Token}`,
+    },
+  })
+    .then(res => res.json())
+    .then(listSubject => {
+      dispatch({
+        type: GET_ALL_SUBJECT,
+        payload: listSubject
+      })
+    })
+};
+
