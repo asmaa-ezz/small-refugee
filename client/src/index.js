eed3ed04b40382731e7a2c95d18041de2fdaadc4
asmaa-ezz <asmadhoun@gmail.com>
2019-06-26 22:39:27 +0300

    Edit some file

--- client/src/index.js
+++ client/src/index.js
@@ -10,6 +10,8 @@ import thunk from 'redux-thunk';
 
 const store = createStore(reducers, applyMiddleware(thunk));
 
+console.log(process.env);
+
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
